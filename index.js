4bd1c92b347fefa68f4ce3c6ece448d25df2763f
manuetov <manuetov@hotmail.com>
2022-07-21 18:14:02 +0200

    acabado

---
+++ index.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+// Import of the model Recipe from './models/Recipe.model.js'
+const Recipe = require('./models/Recipe.model');
+// Import of the data from './data.json'
+const data = require('./data');
+
+const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+// Connection to the database "recipe-app"
+mongoose
+  .connect(MONGODB_URI)
+  .then(x => {
+    console.log(`Connected to the database: "${x.connection.name}"`);
+    // Before adding any recipes to the database, let's remove all existing ones
+    return Recipe.deleteMany()
+  })
+  .then((res) =>{
+    return Recipe.create({
+      "title": "Asian Glazed Chicken Thighs",
+    "level": "Amateur Chef",
+    "ingredients": [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+      "3 tablespoons Asian chili garlic sauce",
+      "3 tablespoons minced garlic",
+      "salt to taste",
+      "8 skinless, boneless chicken thighs"
+    ],
+    "cuisine": "Asian",
+    "dishType": "main_course",
+    "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    "duration": 40,
+    "creator": "Chef LePapu"
+    })
+  }).
+  then((res)=> {
+    return Recipe.deleteMany()
+    // console.log(res)
+  })
+  .then((res) => {
+    // Run your code here, after you have insured that the connection was made
+    return Recipe.insertMany(data)
+  })
+  .then((res) => {
+    // console.log(res);
+  })
+  .then((res) => {
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese" }, {durations: 100}, {new: true} )
+  })
+  .then((res) => {
+    // console.log(res)
+  })
+  .then((res) => {
+    return Recipe.findOneAndDelete({title: "Carrot Cake" })
+  })
+ 
+  
+  .catch(error => {
+    console.error('Error connecting to the database', error);
+  });
+
+mongoose.connection.close(MONGODB_URI)  
